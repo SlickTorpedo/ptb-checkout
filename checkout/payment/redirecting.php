1d970870f33201240b6fb5b7af9dae02162098b0
IkiaeM <64362443+IkiaeM@users.noreply.github.com>
2022-06-07 16:04:29 +0200

    Update redirecting.php

--- checkout/payment/redirecting.php
+++ checkout/payment/redirecting.php
@@ -122,7 +122,6 @@
     $url = $c_webhook;
     $headers = [ 'Content-Type: application/json; charset=utf-8' ];
     $POST = [
-        'username' => 'HG Discount',
         'content' => '**__CHECKOUT ' . $user . '__**
 
 **User ID :** ' . $userid . '
